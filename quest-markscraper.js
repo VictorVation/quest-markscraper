55d357d0c0f48d84691215b5e3461b18d53e776a
Victor Szeto <me@victorszeto.com>
2014-12-22 15:55:23 -0500

    add call for grades from any term

--- quest-markscraper.js
+++ quest-markscraper.js
@@ -1,103 +1,127 @@
-var app = require("express")();
-var request = require("request").defaults({jar: true}); // We need cookies
-var cheerio = require("cheerio");
-var _ = require("lodash");
+var app = require('express')();
+var request = require('request').defaults({jar: true}); // We need cookies
+var cheerio = require('cheerio');
+var _ = require('lodash');
 
 // Replace the following two lines with your username and password
-var username = process.env.qUsername    // "questUsername"
-var password = process.env.qPWD         // "questPassword"
+var username = process.env.QUEST_USER    // 'questUsername';
+var password = process.env.QUEST_PWD     // 'questPassword';
 var base = 'https://quest.pecs.uwaterloo.ca/';
 
-app.get('/', function(req, res) {
-    if( !username || !password) {
-        res.end("Quest username or password is empty.")
-        return;
+var getGrades = function(req, res) {
+	if( !username || !password) {
+    res.end('Quest username or password is empty.')
+    return;
 	}
 
-    var reqOpts =  {
+	var path = req.path.slice(1);
+
+	if (!_.isEmpty(term)) {
+		var term = '';
+
+		switch(_.first(path).toLowerCase()) {
+			case 'f': term += 'Fall '; break;
+			case 'w': term += 'Winter '; break;
+			case 's': term += 'Summer '; break;
+	    default: res.end('Invalid term.'); return;
+		}
+
+		term += '20' + path.slice(1);
+		console.log(term);
+	}
+
+  var reqOpts =  {
 		form: {
 			timezoneOffset: 0,
 			userid: username,
 			pwd: password,
-			Submit: "Sign in",
-			httpPort: ""
+			Submit: 'Sign in',
+			httpPort: ''
 		},
-	    headers: {
-	        'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv:2.0b6) Gecko/20100101 Firefox'
-	    }
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv:2.0b6) Gecko/20100101 Firefox'
+    }
 	}
 
-    // Logging in
-	reqOpts.url = base + "psp/SS/?cmd=login&languageCd=ENG";
+  // Logging in
+	reqOpts.url = base + 'psp/SS/?cmd=login&languageCd=ENG';
 	request.post(reqOpts, function(error, response, body) {
-		console.log("Logged in as " + username)
+		console.log('Logged in as ' + username)
 		reqOpts.form = {};
 
 		// Landing page
-		reqOpts.url = base + "psp/SS/ACADEMIC/SA/h/?tab=DEFAULT";
+		reqOpts.url = base + 'psp/SS/ACADEMIC/SA/h/?tab=DEFAULT';
 		request.get( reqOpts, function(error, response, body) {
-		console.log("Opened landing page")
+		console.log('Opened landing page')
 
 			// Follow redirect
-			reqOpts.url = base + "psp/SS/ACADEMIC/HRMS/c/SA_LEARNER_SERVICES.SSS_STUDENT_CENTER.GBL?FolderPath=PORTAL_ROOT_OBJECT.HC_SSS_STUDENT_CENTER&IsFolder=false&IgnoreParamTempl=FolderPath%2cIsFolder";
+			reqOpts.url = base + 'psp/SS/ACADEMIC/HRMS/c/SA_LEARNER_SERVICES.SSS_STUDENT_CENTER.GBL?FolderPath=PORTAL_ROOT_OBJECT.HC_SSS_STUDENT_CENTER&IsFolder=false&IgnoreParamTempl=FolderPath%2cIsFolder';
 			request.get (reqOpts, function(error, response, body) {
-			console.log("Landing page redirect followed")
+			console.log('Landing page redirect followed')
 
 				// Student center
-				reqOpts.url = base + "psc/SS/ACADEMIC/HRMS/c/SA_LEARNER_SERVICES.SSS_STUDENT_CENTER.GBL?FolderPath=PORTAL_ROOT_OBJECT.HC_SSS_STUDENT_CENTER&amp;IsFolder=false&amp;IgnoreParamTempl=FolderPath%2cIsFolder&amp;PortalActualURL=https%3a%2f%2fquest.pecs.uwaterloo.ca%2fpsc%2fSS%2fACADEMIC%2fHRMS%2fc%2fSA_LEARNER_SERVICES.SSS_STUDENT_CENTER.GBL&amp;PortalContentURL=https%3a%2f%2fquest.pecs.uwaterloo.ca%2fpsc%2fSS%2fACADEMIC%2fHRMS%2fc%2fSA_LEARNER_SERVICES.SSS_STUDENT_CENTER.GBL&amp;PortalContentProvider=HRMS&amp;PortalCRefLabel=Student%20Center&amp;PortalRegistryName=ACADEMIC&amp;PortalServletURI=https%3a%2f%2fquest.pecs.uwaterloo.ca%2fpsp%2fSS%2f&amp;PortalURI=https%3a%2f%2fquest.pecs.uwaterloo.ca%2fpsc%2fSS%2f&amp;PortalHostNode=SA&amp;NoCrumbs=yes"
+				reqOpts.url = base + 'psc/SS/ACADEMIC/HRMS/c/SA_LEARNER_SERVICES.SSS_STUDENT_CENTER.GBL?FolderPath=PORTAL_ROOT_OBJECT.HC_SSS_STUDENT_CENTER&amp;IsFolder=false&amp;IgnoreParamTempl=FolderPath%2cIsFolder&amp;PortalActualURL=https%3a%2f%2fquest.pecs.uwaterloo.ca%2fpsc%2fSS%2fACADEMIC%2fHRMS%2fc%2fSA_LEARNER_SERVICES.SSS_STUDENT_CENTER.GBL&amp;PortalContentURL=https%3a%2f%2fquest.pecs.uwaterloo.ca%2fpsc%2fSS%2fACADEMIC%2fHRMS%2fc%2fSA_LEARNER_SERVICES.SSS_STUDENT_CENTER.GBL&amp;PortalContentProvider=HRMS&amp;PortalCRefLabel=Student%20Center&amp;PortalRegistryName=ACADEMIC&amp;PortalServletURI=https%3a%2f%2fquest.pecs.uwaterloo.ca%2fpsp%2fSS%2f&amp;PortalURI=https%3a%2f%2fquest.pecs.uwaterloo.ca%2fpsc%2fSS%2f&amp;PortalHostNode=SA&amp;NoCrumbs=yes'
 				request.get(reqOpts, function(error, response, body) {
-				console.log("Clicked \"Student Center\"")
+				console.log('Clicked \'Student Center\'')
 
-					// "My Academics" POST
-					reqOpts.url = base + "psc/SS/ACADEMIC/HRMS/c/SA_LEARNER_SERVICES.SSS_STUDENT_CENTER.GBL";
+					// 'My Academics' POST
+					reqOpts.url = base + 'psc/SS/ACADEMIC/HRMS/c/SA_LEARNER_SERVICES.SSS_STUDENT_CENTER.GBL';
 					reqOpts.form = {
 				    	ICAction:'DERIVED_SSS_SCR_SSS_LINK_ANCHOR1',
 					}
 					request.post(reqOpts, function(error, response, body) {
-						console.log("Clicked \"My Academics\"")
+						console.log('Clicked \'My Academics\'')
 						reqOpts.form = {};
 
 						// Following redirect
-						reqOpts.url = base + "psc/SS/ACADEMIC/HRMS/c/UW_SS_MENU.UW_SS_MYPROG_UG.GBL?Page=UW_SS_MYPROG_UG&Action=U&ExactKeys=Y";
+						reqOpts.url = base + 'psc/SS/ACADEMIC/HRMS/c/UW_SS_MENU.UW_SS_MYPROG_UG.GBL?Page=UW_SS_MYPROG_UG&Action=U&ExactKeys=Y';
 						request.get(reqOpts, function(error, response, body) {
-						console.log("\"My Academics\" redirect followed")
+						console.log('\'My Academics\' redirect followed')
 
-							// Click "Grades"
-							reqOpts.url = base + "psc/SS/ACADEMIC/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_GRADE.GBL?Page=SSR_SSENRL_GRADE&Action=A";
+							// Click 'Grades'
+							reqOpts.url = base + 'psc/SS/ACADEMIC/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_GRADE.GBL?Page=SSR_SSENRL_GRADE&Action=A';
 							request.get(reqOpts, function(error, response, body) {
-								console.log("Clicked \"Grades\"")
+								console.log('Clicked \'Grades\'')
 
-								// Select current term
-								reqOpts.url = base + "psc/SS/ACADEMIC/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_GRADE.GBL"
+								var $ = cheerio.load(body);
+
+								// Select current term using terrible dom/regex/whatever hack
+								var re = /value="(\d)/
+								var termNumber = _.isEmpty(term)
+									? '2'
+									: re.exec($('tr:contains(' + term + ')').last().children().children().html())[1];
+
+								reqOpts.url = base + 'psc/SS/ACADEMIC/HRMS/c/SA_LEARNER_SERVICES.SSR_SSENRL_GRADE.GBL'
 								reqOpts.form = {
-									ICAction : "DERIVED_SSS_SCT_SSR_PB_GO",
-									SSR_DUMMY_RECV1$sels$0 : 1	// Current term
+									ICAction : 'DERIVED_SSS_SCT_SSR_PB_GO',
+									SSR_DUMMY_RECV1$sels$0: termNumber
 								}
 								request.post(reqOpts, function(error, response, body) {
-									console.log("Term selected")
+									console.log('Term selected')
 									var classes = []
 									var grades = []
+
 									var $ = cheerio.load(body);
 									$('a.PSHYPERLINK').each( function(i,element) {
 										classes.push( $(this).text() )
 									})
-									console.log ("Classes scraped")
+									console.log ('Classes scraped')
 									$('.PABOLDTEXT').each( function(i,element) {
 										grades.push( $(this).text() )
 									})
-									console.log ("Grades scraped")
+									console.log ('Grades scraped')
     								classes.shift();
 									var allAvailable = (classes.length == grades.length && classes.length > 1);
 									var gradeObj = _.object(classes, grades);
 									if (allAvailable)
-										returnJSON = _.extend({"allAvailable" : allAvailable}, {grades: gradeObj});
+										returnJSON = _.extend({'allAvailable' : allAvailable}, {grades: gradeObj});
 									else
-										returnJSON = {"allAvailable" : allAvailable}
+										returnJSON = {'allAvailable' : allAvailable}
 
                                     res.setHeader('Access-Control-Allow-Origin', '*');
   									res.send(returnJSON);
 
-                                    console.log("Response sent");
+                                    console.log('Response sent');
                                     return;
 								})
 							})
@@ -106,10 +130,13 @@ app.get('/', function(req, res) {
 				})
 			})
 		})
-	});
-});
+	})
+}
+
+app.get('/', getGrades);
+app.get('/(^$|[fwsFWS]{1}\\d{2}$)', getGrades);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-console.log("Listening on " + port);
+console.log('Listening on ' + port);
 });
